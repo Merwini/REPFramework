a083e887c32f78c09cc37676587df8931e3b4904
Merwini <86023141+Merwini@users.noreply.github.com>
2024-05-25 23:09:11 -0400

    CE and vanilla version of CompAntiarmor

---
+++ CombatExtended/Source/CE Compat/CompAntiarmorCE.cs
@@ -0,0 +1,65 @@
+﻿using CombatExtended;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+using rep.heframework;
+
+namespace rep.heframework.cecompat
+{
+    public class CompAntiarmorCE : CompAntiarmor
+    {
+        public CompProperties_AntiarmorCE Props
+        {
+            get
+            {
+                return (CompProperties_AntiarmorCE)props;
+            }
+        }
+
+        public override float GetPawnOverallArmor(Pawn pawn, StatDef stat)
+        {
+            //from RebuildArmorCache(Dictionary<BodyPartRecord, float> armorCache, StatDef stat)
+            Dictionary<BodyPartRecord, float> armorCache = new Dictionary<BodyPartRecord, float>();
+            float naturalArmor = pawn.GetStatValue(stat);
+            List<Apparel> wornApparel = pawn.apparel?.WornApparel;
+            foreach (BodyPartRecord part in pawn.RaceProps.body.AllParts)
+            {
+                //TODO: 1.5 should be Neck
+                if (part.depth == BodyPartDepth.Outside && (part.coverage >= 0.1 || (part.def == BodyPartDefOf.Eye || part.def == BodyPartDefOf.Eye)))
+                {
+                    float armorValue = part.IsInGroup(CE_BodyPartGroupDefOf.CoveredByNaturalArmor) ? naturalArmor : 0f;
+                    if (wornApparel != null)
+                    {
+                        foreach (var apparel in wornApparel)
+                        {
+                            if (apparel.def.apparel.CoversBodyPart(part))
+                            {
+                                armorValue += apparel.PartialStat(stat, part);
+                            }
+                        }
+                    }
+                    armorCache[part] = armorValue;
+                }
+            }
+
+            //from TryDrawOverallArmor(Dictionary<BodyPartRecord, float> armorCache, ref float curY, float width, StatDef stat, string label, string unit)
+            float averageArmor = 0;
+            float bodyCoverage = 0;
+            foreach (var bodyPartValue in armorCache)
+            {
+                BodyPartRecord part = bodyPartValue.Key;
+                float armorValue = bodyPartValue.Value;
+                averageArmor += armorValue * part.coverage;
+                bodyCoverage += part.coverage;
+            }
+            averageArmor /= bodyCoverage;
+
+            return averageArmor;
+        }
+    }
+}
---
+++ CombatExtended/Source/CE Compat/CompProperties_AntiarmorCE.cs
@@ -0,0 +1,21 @@
+﻿using CombatExtended;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+using rep.heframework;
+
+namespace rep.heframework.cecompat
+{
+    public class CompProperties_AntiarmorCE : CompProperties_Antiarmor
+    {
+        public CompProperties_AntiarmorCE()
+        {
+            this.compClass = typeof(CompAntiarmorCE);
+        }
+    }
+}

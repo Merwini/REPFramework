fb9d34ca4f9d246390441fc5b92f0f73ab3d2ebc
Merwini <86023141+Merwini@users.noreply.github.com>
2024-09-30 09:07:03 -0400

    Experimental ThinkTree substitution

---
+++ Source/DefModExtensions/PawnKindExtension.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+
+namespace rep.heframework
+{
+    public class PawnKindExtension : DefModExtension
+    {
+        ThinkTreeDef thinkTree;
+
+        ThinkTreeDef constantThinkTree;
+
+        public ThinkTreeDef ThinkTree => thinkTree;
+
+        public ThinkTreeDef ConstantThinkTree => constantThinkTree;
+
+        public static Dictionary<Pawn, ThinkTreeDef> thinkDict = new Dictionary<Pawn, ThinkTreeDef>();
+
+        public static Dictionary<Pawn, ThinkTreeDef> constantThinkDict = new Dictionary<Pawn, ThinkTreeDef>();
+    }
+
+
+}
